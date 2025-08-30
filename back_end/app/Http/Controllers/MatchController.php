db6f14dc140b97311cd0535dd26c2ba3fc810fb5
Mic-9 <116760683+Mic-9@users.noreply.github.com>
2025-08-30 02:11:02 +0200

    matches routes pt1

---
+++ back_end/app/Http/Controllers/MatchController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RugbyMatch;
+use Illuminate\Http\Request;
+
+class MatchController extends Controller
+{
+    public function index()
+    {
+        $matches = RugbyMatch::all();
+        return response()->json($matches);
+    }
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'api_id' => 'required|unique:matches|string|max:255',
+            'match_date' => 'required|date_format:Y-m-d H:i:s',
+            'team_1' => 'required|string',
+            'team_2' => 'required|string',
+            'status' => 'required|string',
+            'team_1_score' => 'nullable|integer',
+            'team_2_score' => 'nullable|integer',
+        ]);
+
+        $match = RugbyMatch::create($validatedData);
+        return response()->json([
+            'message' => 'Match created successfully',
+            'match' => $match
+        ], 201);
+    }
+}
--- back_end/routes/api.php
+++ back_end/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\MatchController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,6 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
+
+Route::get('/matches', [MatchController::class, 'index']);
+Route::middleware('auth:sanctum')->post('/matches', [MatchController::class, 'store']);
